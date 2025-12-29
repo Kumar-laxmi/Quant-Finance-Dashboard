a895009b67d82a451226f0cd52b5d9187b6dadc7
Kumar Laxmikant <kumarlaxmikant@Kumars-MacBook-Pro-593.local>
2025-12-29 10:00:35 +0100

    Candle Chart integration for US ETFs completed

--- app/views.py
+++ app/views.py
@@ -2,6 +2,65 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import plotly.graph_objects as go
+from plotly.offline import plot
+
 def index(request):
-    return render(request, 'index.html', {})
+    # Download ETF data
+    data_etf = yf.download(
+        # passes the ticker
+        tickers=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VXX',          # <- US ETFs
+                'FEZ', 'IEUR', 'EWG', 'EWQ', 'EWU', 'EWL'],        # <- European ETFs
+        group_by = 'ticker',
+        threads=True, # Set thread value to true
+        # used for access data[ticker]
+        period='1d', 
+        interval='1m' 
+    )
+
+    def make_candle_chart(df, ticker):
+        if df.empty:
+            return None
+
+        fig = go.Figure(
+            data=[go.Candlestick(
+                x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'],
+                name=ticker
+            )]
+        )
+
+        fig.update_layout(
+            paper_bgcolor="#14151b",
+            plot_bgcolor="#14151b",
+            font_color="white",
+            xaxis_rangeslider_visible=False,
+            height=250,
+            margin=dict(l=5, r=5, t=20, b=5)
+        )
+        return plot(fig, auto_open=False, output_type='div')
+
+    ETF_REGIONS = {
+        "US": ['SPY','QQQ','DIA','IWM','VTI','VXX'],
+        "EUROPE": ['FEZ','IEUR','EWG','EWQ','EWU','EWL']
+    }
+
+    plots = {}
+
+    for region, tickers in ETF_REGIONS.items():
+        plots[region] = {}
+        for ticker in tickers:
+            df = data_etf[ticker].dropna()
+            plots[region][ticker] = make_candle_chart(df, ticker)
+
+    return render(request, 'index.html', {
+        'plots': plots
+    })
+
 
